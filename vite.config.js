23f28388f9035788cf4ad74f85219483965f812d
Christine Brovkina <christine@whateverism.de>
2024-11-02 16:00:10 +0100

    add config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	base: '/under-deconstruction/', // Replace <REPO_NAME> with your repository name
+	build: {
+		outDir: 'dist',
+	},
+});
